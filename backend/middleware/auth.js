7f695cb1ffd4d311dcbe5a241bc217f29d70515c
ireene-hazwani <s75702@ocean.umt.edu.my>
2026-06-09 13:22:05 +0800

    Delete backend directory

--- backend/middleware/auth.js
+++
@@ -1,4 +0,0 @@
-exports.verifyAccess = (req, res, next) => {
-    console.log(`[SECURITY LOG]: Incoming request to ${req.path}`);
-    next();
-};
--- backend/middleware/authMiddleware.js
+++
@@ -1,7 +0,0 @@
-// This middleware can be expanded later to verify JWT tokens or session cookies
-exports.verifyAccess = (req, res, next) => {
-    console.log(`[SECURITY LOG]: Incoming request to ${req.path}`);
-    
-    // For now, it cleanly passes the request forward to the controllers
-    next();
-};
